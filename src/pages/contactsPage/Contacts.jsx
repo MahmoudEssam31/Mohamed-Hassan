747969623f84aba55ff1e914a5a7afcd1bcb4fb1
MahmoudEssam31 <mabounar77@gmail.com>
2025-09-09 01:40:39 +0300

    Fix13

--- src/pages/contactsPage/Contacts.jsx
+++ src/pages/contactsPage/Contacts.jsx
@@ -12,8 +12,8 @@ export default function Contacts() {
           <HeaderComponant title={"Contacts Me"} img={`${import.meta.env.BASE_URL}PersonlImg/imgo1.svg`} />
           <CustomContainer>
               <div className="contacts">
-                  <div className="contacts-contact flex flex-col md:gap-[1.5rem] gap-[1.1rem] justify-center mt-[4.5rem] text-[1.5rem] font-[600]">
-                      <a href="tel:+201000651512" className="contacts-contact-number flex items-center gap-[1.5rem] group">
+                  <div className="contacts-contact flex flex-col gap-[1.5rem]  justify-center mt-[4.5rem] text-[1.5rem] font-[600]">
+                      <a href="tel:+201000651512" className="contacts-contact-number flex items-center md:gap-[1.5rem] gap-[1.1rem] group">
                           <a className="  flex justify-center items-center text-[1.5rem] h-[3.5rem] w-[3.5rem] rounded-4xl text-[var(--main-color)] bg-[var(--background-color)] group duration-300">
                               <IoCall className="group-hover:scale-110 group-hover:rotate-15 duration-300, group-hover:text-[var(--yellow-color)]" />
                           </a>

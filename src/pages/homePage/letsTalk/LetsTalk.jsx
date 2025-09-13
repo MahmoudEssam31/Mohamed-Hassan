cd7f86c98ba3f878335b30b554fe3739c268547b
MahmoudEssam31 <mabounar77@gmail.com>
2025-09-13 23:54:02 +0300

    Fix 15

--- src/pages/homePage/letsTalk/LetsTalk.jsx
+++ src/pages/homePage/letsTalk/LetsTalk.jsx
@@ -6,6 +6,7 @@ export default function LetsTalk({ classNamecontainer, classNametext, classNameI
         <div
             className={`lets-talk flex md:flex-row flex-col justify-center items-center bg-[var(--service-color)] h-[59.37rem] lg:-mx-[56px] md:-mx-[44px] -mx-[20px] md:mt-[7rem] mt-[3rem] ${classNamecontainer}`}>
             <div
+                data-aos="fade-up-right"
                 className={`lets-talt-text flex flex-col justify-center items-center lg:w-[31.25rem] lg:h-[31.25rem] md:w-[21.59rem] md:h-[21.59rem] w-[21.59rem] h-[21.59rem] bg-[var(--main-color)]  md:rounded-[50%] rounded-2xl relative lg:top-[-4.5rem] lg:left-[1.8rem] md:top-[6.1rem] md:left-[4.8rem] top-[rem] left-[.01rem] gap-[1.5rem] ${classNametext}`}>
                 <p className="font-[600] text-[var(--background-color)] text-[.97rem]">Have a great idea?</p>
                 <h2 className="lg:text-[2.18rem] md:text-[2rem] text-[2.1rem] lg:leading-[2em] md:leading-[1.5em] leading-[1.8em] md:font-[400] font-[400] font-[Staatliches] uppercase text-center md:w-[70%] w-[60%] ">
@@ -18,12 +19,13 @@ export default function LetsTalk({ classNamecontainer, classNametext, classNameI
                 </a>
             </div>
             <div
+                data-aos="fade-up-left"
                 className={`lets-talt-img lg:w-[25.25rem] lg:h-[25.25rem] md:w-[15.59rem] md:h-[15.59rem] w-[15.59rem] h-[15.59rem] bg-[var(--yellow-color)] rounded-[50%] relative lg:top-[4.5rem] lg:left-[-1.5rem] md:top-[15.5rem] md:right-[-1.9rem] top-[3.5rem]  left-[.01rem] overflow-hidden ${classNameImg}`}>
-                 <GiSharkFin
-                                  size={250}
-                                  fill="var(--background-color)"
-                                  className="absolute lg:h-[25rem] lg:w-[19rem] md:h-[15rem] md:w-[11rem] h-[17rem] w-[12.3rem] lg:bottom-[-2rem] lg:right-[3.4rem] md:bottom-[-1.5rem] md:right-[2.6rem] bottom-[-1.5rem] right-[2.1rem]"
-                              />
+                <GiSharkFin
+                    size={250}
+                    fill="var(--background-color)"
+                    className="absolute lg:h-[25rem] lg:w-[19rem] md:h-[15rem] md:w-[11rem] h-[17rem] w-[12.3rem] lg:bottom-[-2rem] lg:right-[3.4rem] md:bottom-[-1.5rem] md:right-[2.6rem] bottom-[-1.5rem] right-[2.1rem]"
+                />
             </div>
         </div>
     );
